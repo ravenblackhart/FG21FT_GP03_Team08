4ba56560ef9708b8f824487dd391d0ea503d9a53
Raven Blackhart <raven@cafiend.com>
2022-06-20 17:06:43 +0200

    Sync

--- Source/Zom/FuncLib/ZomEquationBPLib.cpp
+++ Source/Zom/FuncLib/ZomEquationBPLib.cpp
@@ -7,5 +7,5 @@ UZomEquationBPLib::UZomEquationBPLib()
 
 float UZomEquationBPLib::RamEquation(int n)
 {
-	return (1.f - FMath::Pow(1.f / (n + 1.f), 0.6f)) / 0.69f;
+	return (1.f - FMath::Pow(n + 1.f, -0.6f)) / 0.69f;
 }
